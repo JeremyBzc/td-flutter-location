d34d7d29bef9820985262a68c4b68f50c0fad9a9
Jérémy <bruzacj@yahoo.fr>
2023-02-05 14:43:23 +0100

    fix habitation constructor and add option constructor

--- lib/models/habitation.dart
+++ lib/models/habitation.dart
@@ -41,11 +41,17 @@ class Option {
   String description;
 
   Option(this.id, this.libelle, {this.description = ""});
+  Option.fromJson(Map<String, dynamic> json)
+    : id = json['id'],
+      libelle = json['libelle'],
+      description = json['description'];
 }
-
 class OptionPayante extends Option {
   double prix;
 
   OptionPayante(super.id, super.libelle, {super.description = "", this.prix = 0});
+  OptionPayante.fromJson(Map<String, dynamic> json)
+    : prix = json['prix'],
+    super.fromJson(json['optionpayante']);
 }
 
