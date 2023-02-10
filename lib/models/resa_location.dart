2e23c3bc094e04d4fb37bc7e663f325cdb1d5f40
Jérémy <bruzacj@yahoo.fr>
2023-02-10 16:47:39 +0100

    add buildResume widget and first version buildDates

--- lib/models/resa_location.dart
+++ lib/models/resa_location.dart
@@ -3,8 +3,10 @@ import 'package:location/models/habitation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:location/views/share/date_widget.dart';
 
 import '../share/location_text_style.dart';
+
 
 class ResaLocation extends StatefulWidget {
 
@@ -24,6 +26,9 @@ class _ResaLocationState extends State<ResaLocation> {
   //int prixTotal = 0;
   List<OptionPayanteCheck> optionPayanteChecks = [];
 
+  final TextEditingController startDateController = TextEditingController(),
+      endDateController = TextEditingController();
+
   var format = NumberFormat("### €");
 
   // get prixTotal => null;
@@ -41,7 +46,7 @@ class _ResaLocationState extends State<ResaLocation> {
         padding: EdgeInsets.all(4.0),
         children: [
           _buildResume(),
-          //_buildDates(),
+          _buildDates(),
           //_buildNbPersonnes(),
           //_buildOptionsPayantes(context),
           //TotalWidget(prixTotal),
@@ -55,7 +60,7 @@ class _ResaLocationState extends State<ResaLocation> {
       margin: EdgeInsets.all(2),
       child: Material(
         child: ListTile(
-          leading: Icon(Icons.house),
+          leading: Icon(Icons.house_rounded),
           title: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -73,6 +78,73 @@ class _ResaLocationState extends State<ResaLocation> {
       ),
     );
   }
+  _buildDates() {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding:  EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(width: 2.0, color: const Color(0xFFFFFFFF)),
+      ),
+      child: GestureDetector (
+        onTap:() {
+          dateTimeRangePicker();
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.calendar_today_outlined,
+              size: 20,
+              color: Colors.grey,
+            ),
+            SizedBox(width: 30),
+            Text(DateFormat('dd MMM yyyy', 'fr').format(dateDebut),
+              style: TextStyle(
+              fontSize: 16,
+              //fontWeight: LocationTextStyle.boldTextStyle,
+              )
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.blue.shade900,
+              child: Icon(Icons.arrow_right),
+            ),
+            SizedBox(width: 30),
+            Icon(
+              Icons.calendar_today_outlined,
+              size: 20,
+              color: Colors.black,
+            ),
+            SizedBox(width: 20),
+            Text(DateFormat('dd MMM yyyy', 'fr').format(dateFin),
+              style: TextStyle(
+                fontSize: 16,
+                //fontWeight: LocationTextStyle.boldTextStyle,
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  dateTimeRangePicker() async {
+    DateTimeRange? datePicked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(DateTime.now().year),
+      lastDate: DateTime(DateTime.now().year + 2),
+      initialDateRange: DateTimeRange(start: dateDebut, end: dateFin),
+      cancelText: 'Annuler',
+      confirmText: 'Valider',
+      locale: const Locale("fr", "FR"),
+    );
+    if (datePicked != null) {
+      setState(() {
+        dateDebut = datePicked.start;
+        dateFin = datePicked.end;
+        //DateFormat('dd MMM yyyy', 'fr').format(dateDebut)
+      });
+    }
+  }
+
 
 }
 
---
+++ lib/views/share/date_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:location/models/resa_location.dart';
+
+class DateWidget extends StatelessWidget {
+  final DateTime dateDebut;
+  final DateTime dateFin;
+
+  const DateWidget(this.dateDebut, this.dateFin, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.calendar_today_outlined,
+          size: 20,
+          color: Colors.grey,
+        ),
+        SizedBox(width: 30),
+        Text(DateFormat('dd MMM yyyy', 'fr').format(dateDebut),
+            style: TextStyle(
+              fontSize: 16,
+            )
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.blue.shade900,
+          child: Icon(Icons.arrow_right),
+        ),
+        SizedBox(width: 30),
+        Icon(
+          Icons.calendar_today_outlined,
+          size: 20,
+          color: Colors.black,
+        ),
+        SizedBox(width: 20),
+        Text(DateFormat('dd MMM yyyy', 'fr').format(dateFin),
+            style: TextStyle(
+              fontSize: 16,
+            )
+        ),
+      ],
+    );
+  }
+}
